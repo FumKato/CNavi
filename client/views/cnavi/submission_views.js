32d8ce7bf03a88d782b965912186f51d55b4c983
加藤 史也 <FumKATO@katou-no-MacBook-Air.local>
2014-06-06 15:14:28 +0900

    Add submission view

--- client/views/cnavi/submission_views.js
+++ client/views/cnavi/submission_views.js
@@ -1,5 +1,7 @@
-Template.submission.helpers({
-	get_lesson_name : function(){
+SubmissionView = function(){
+	var _this = SubmissionView;
+	
+	_this.prototype.get_lesson_name = function(){
 		var lesson = lessons_model.get_lessons_by_id(Session.get('lesson_id'));
 		if(lesson == null) return;
 		if(Session.get('myself').role == 'teacher' || Session.get('myself').role == 'assistant'){
@@ -8,15 +10,9 @@ Template.submission.helpers({
 			return lesson.name + ': ' + submission.user_name;
 		}
 		return lesson.name;
-	},
+	};
 	
-	get_questions: function(){
-		var questions = questions_model.get_questions_by_lesson_id(Session.get('lesson_id'));
-		if(questions.length == 0) return;
-		return questions[0].questions;
-	},
-	
-	render_answer: function(num, question){
+	_this.prototype.render_answer = function(num, question){
 		if(Session.get('student_id') != null && (Session.get('myself').role == 'teacher' || Session.get('myself').role == 'assistant')){
 			var questions = questions_model.get_questions_by_lesson_id(Session.get('lesson_id'));
 			var submissions = submissions_model.get_submissions_by_user_id(Session.get('lesson_id'), Session.get('student_id'));
@@ -26,5 +22,23 @@ Template.submission.helpers({
 			return answers;
 		}
 		return '<div class="questionAnswerForm"> <input type="text" name=" + ' + question + '"/></div>';
+	};
+};
+
+submission_view = new SubmissionView();
+
+Template.submission.helpers({
+	get_lesson_name : function(){
+		return submission_view.get_lesson_name();
+	},
+	
+	get_questions: function(){
+		var questions = questions_model.get_questions_by_lesson_id(Session.get('lesson_id'));
+		if(questions.length == 0) return;
+		return questions[0].questions;
+	},
+	
+	render_answer: function(num, question){
+		return submission_view.render_answer(num, question);
 	}
 });
